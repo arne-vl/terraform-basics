2d9400d9cf68a81e40746a2c7565e653d71c8d23
Arne Van Looveren <arne.vanlooveren@epicdatacronos.onmicrosoft.com>
2025-02-17 20:24:20 +0100

    Code folder

---
+++ code/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.naam}-terraform-basics"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "st${var.naam}terraformbasics"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "st${var.naam}blobcontainer"
+  storage_account_id  = azurerm_storage_account.storage.id
+  container_access_type = "private"
+}
---
+++ code/variables.tf
@@ -0,0 +1,9 @@
+variable "naam" {
+  description = "Naam van de persoon om resource namen te genereren."
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9]+$", var.naam))
+    error_message = "De naam moet in kleine letters zijn en mag geen streepjes of spaties bevatten."
+  }
+}
